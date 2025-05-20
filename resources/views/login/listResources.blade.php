f8d3a0ba29ee7eb1bc9f78b70e0502b105f0081e
Mohamed Reda EL ABOUDI <152705321+MohamedRedaElaboudi@users.noreply.github.com>
2025-05-20 22:12:11 +0100

    we add a resources list

---
+++ resources/views/login/listResources.blade.php
@@ -0,0 +1,31 @@
+<x-navbar></x-navbar>
+
+<div class="max-w-2xl mx-auto mt-6 space-y-4 font-sans mb-4">
+  <!-- Boîte pour annoncer quelque chose -->
+  <div class="bg-white dark:bg-gray-900 flex items-center gap-4 bg-white shadow rounded-lg p-4">
+    <input type="text" placeholder="Annoncez quelque chose à votre classe" class="flex-1 border border-gray-300 rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500">
+  </div>
+
+  <!-- Bloc de support de cours -->
+  <div class="bg-white dark:bg-gray-900 shadow rounded-lg p-4">
+    <div class="flex items-center gap-3 bg-white dark:bg-gray-900">
+      <div class="bg-gray-200 p-2 rounded-full">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 20h9" />
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4h9" />
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12h18" />
+        </svg>
+      </div>
+      <div>
+        <p class="font-medium">Mohammed KASRI a publié un nouveau support de cours : <span class="text-blue-600">Cours_SDA_10</span></p>
+        <p class="text-sm text-gray-500">7 mai</p>
+      </div>
+    </div>
+  </div>
+
+ 
+  </div>
+</div>
+
+<footer><x-footer></x-footer>
+ </footer>
--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,6 @@ Route::post('/rejoindre', [CoursController::class, 'rejoindre'])->name('cours.re
 
 // Cours : voir les classes inscrites
 Route::get('/mescours', [CoursController::class, 'mesCoursInscrits'])->name('cours.mescours');
+Route::get('/mesres', function () {
+    return view('login.listResources');
+});
